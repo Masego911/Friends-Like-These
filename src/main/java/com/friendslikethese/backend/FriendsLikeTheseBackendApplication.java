4bded5e68fe3cbadab014cfa38766a04bebe6ad5
Masego <masego.mogoshadi@gmail.com>
2026-08-22 16:38:26 +0200

    Initial Friends Like These backend setup

---
+++ src/main/java/com/friendslikethese/backend/FriendsLikeTheseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.friendslikethese.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendsLikeTheseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendsLikeTheseBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/friendslikethese/backend/FriendsLikeTheseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.friendslikethese.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendsLikeTheseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
